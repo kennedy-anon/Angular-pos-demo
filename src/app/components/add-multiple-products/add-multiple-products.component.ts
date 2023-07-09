628dd339a7ea7656deb4da3db9af3ea43504f30f
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-07-09 21:33:02 +0300

    saving multiple products

--- src/app/components/add-multiple-products/add-multiple-products.component.ts
+++ src/app/components/add-multiple-products/add-multiple-products.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProductNameComponent } from 'src/app/dialogs/edit-product-name/edit-product-name.component';
 import { AddMultipleProductsService } from 'src/app/services/add-multiple-products.service';
+import { ProductsService } from 'src/app/services/products.service';
 import { SnackBarCustomService } from 'src/app/services/snack-bar-custom.service';
 
 @Component({
@@ -13,8 +14,10 @@ export class AddMultipleProductsComponent {
   productName !: string;
   products : any = [];
   displayedColumns = ['no', 'product_name', 'actions'];
+  saveComplete = false;
 
-  constructor(private addMultipleProductsService: AddMultipleProductsService, public dialog: MatDialog, private _snackBar: SnackBarCustomService) {}
+  constructor(private addMultipleProductsService: AddMultipleProductsService, public dialog: MatDialog, private _snackBar: SnackBarCustomService, 
+    private productsService: ProductsService) {}
 
   openDialog(product: any): void {
     const dialogRef = this.dialog.open(EditProductNameComponent, {
@@ -53,7 +56,36 @@ export class AddMultipleProductsComponent {
 
   // save products
   saveProducts() {
-    
+    var productCount = this.products.length;
+    for (let product in this.products) {
+      const productDetail = {
+        product_name: this.products[product],
+        min_selling_price: null
+      }
+
+      this.productsService.addProductService(productDetail).subscribe({
+        next: (res =>{
+          res.status == 201 ? this.addMultipleProductsService.deleteProduct((res.body as any)?.product_name): undefined;
+          productCount -= 1;
+          productCount == 0 ? this.saveComplete = true : undefined;
+        }),
+        error: (err =>{
+          if (err.status == 400) {
+            this._snackBar.showErrorMessage(err.error.product_name + " - " + err.error.detail);
+          } else if (err.status == 403) {
+            this._snackBar.showErrorMessage("Session expired. Kindly login again.");
+          }
+          productCount -= 1;
+          productCount == 0 ? this.saveComplete = true : undefined;
+        })
+      });
+
+    }
+  }
+
+  // hides the saving complete message displayed after products are saved
+  hideSaveMessage() {
+    this.saveComplete =false;
   }
 
   ngOnInit(): void {
