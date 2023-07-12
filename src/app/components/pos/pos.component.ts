f919150df09c12c8502b47110cbd65ffd2eb6225
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-07-13 00:12:37 +0300

    designing POS

--- src/app/components/pos/pos.component.ts
+++ src/app/components/pos/pos.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+  'HQC4LP3GHT',
+  '1027660e333f8d26fcb7aad4eb0b8306'
+);
 
 @Component({
   selector: 'app-pos',
@@ -6,5 +12,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./pos.component.css']
 })
 export class PosComponent {
+
+  config = {
+    indexName: 'dev_DenloyPOS_dev_DenloyPOS',
+    searchClient
+  };
+
+  displayedColumns = ['no', 'product_name', 'units', 'unit_price', 'sub_total', 'actions'];
+  products : any = [];
+
+  constructor() {}
+
+  // selected product
+  selectProduct(product: any) {
+
+  }
 
 }
