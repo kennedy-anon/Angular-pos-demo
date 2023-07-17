e2c0e97e4d89979027ca2bf2007de3c600f759fe
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-07-17 21:59:06 +0300

    designing credit sales table

--- src/app/components/credit-sales/credit-sales.component.ts
+++ src/app/components/credit-sales/credit-sales.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./credit-sales.component.css']
 })
 export class CreditSalesComponent {
-
+  displayedColumns = ['no', 'created_at', 'customer_name', 'invoice_amount', 'invoice_paid', 'invoice_balance', 'customer_contact_no', 'actions'];
+  credit_sales : any = [];
 }
